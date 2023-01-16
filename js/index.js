80da60640e0481665583ab7488a0f1c53c42a32c
Andy <andreas.szabo.1@gmail.com>
2023-01-16 14:56:54 +0200

    added html form

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const submit = document.querySelector('#submit');
+    const type = document.querySelector('#typeInput');
+    // submit.removeAttribute('disabled');
+
+
+    // TODO: do stuff
+});
