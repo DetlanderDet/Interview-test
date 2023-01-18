12d24d4876d9080648fb321c3e939902f4e30536
Andy <andreas.szabo.1@gmail.com>
2023-01-18 16:48:41 +0200

    Added form validation

--- js/index.js
+++ js/index.js
@@ -10,12 +10,15 @@ class Ready {
         this.titleInput = document.querySelector("#titleInput");
         this.abstractInput = document.querySelector("#abstractInput");
         this.authorInputs = [document.querySelector("#authorInput1")];
+        this.submitButton = document.querySelector("#submit");
+        this.uploadInput = document.querySelector("#fileInput");
     }
 
     init() {
         this.step1();
         this.addAuthorInput();
         this.removeAuthorInput();
+        this.submitForm();
 
         this.submit.removeAttribute("disabled");
     }
@@ -23,6 +26,7 @@ class Ready {
         const button = this.document.querySelector("#addauthorbutton");
         button.addEventListener("click", () => {
             this.authorsNo++;
+
             const newAuthorDiv = document.createElement("div");
             newAuthorDiv.setAttribute("class", "form-floating mb-3");
 
@@ -36,14 +40,74 @@ class Ready {
             const newAuthorLabel = document.createElement("label");
             newAuthorLabel.setAttribute("for", `authorInput${this.authorsNo}`);
             newAuthorLabel.innerHTML = "Author Name";
+
+            const invalidFeedback = document.createElement("div");
+            invalidFeedback.setAttribute("class", "invalid-feedback");
+            invalidFeedback.innerHTML = "Author name must be between 1 and 50 characters.";
+            
             newAuthorDiv.appendChild(newAuthorInput);
             newAuthorDiv.appendChild(newAuthorLabel);
+            newAuthorDiv.appendChild(invalidFeedback);
             this.document
                 .querySelector("#authorlist")
                 .appendChild(newAuthorDiv);
         });
     }
 
+    submitForm() {
+        this.submitButton.addEventListener("click", (e) => {
+            console.log(e);
+            e.preventDefault();
+            let result = this.validateForm();
+            console.log(result);
+        });
+    }
+
+    validateForm() {
+        let result = true;
+        if (this.titleInput.value.length > 0 && this.titleInput.value.length <= 255) {
+            this.titleInput.classList.add("is-valid");
+            this.titleInput.classList.remove("is-invalid");
+        }else {
+            this.titleInput.classList.add("is-invalid");
+            this.titleInput.classList.remove("is-valid");
+            result = false;
+        }
+
+        if (this.abstractInput.value.length > 0){
+            this.abstractInput.classList.add("is-valid");
+            this.abstractInput.classList.remove("is-invalid");
+        }else {
+            this.abstractInput.classList.add("is-invalid");
+            this.abstractInput.classList.remove("is-valid");
+            result = false;
+        }
+
+        this.authorInputs.forEach((authorInput) => {
+            if (authorInput.value.length > 0 && authorInput.value.length <= 50) {
+                authorInput.classList.add("is-valid");
+                authorInput.classList.remove("is-invalid");
+            } else {
+                authorInput.classList.add("is-invalid");
+                authorInput.classList.remove("is-valid");
+                result = false;
+            }
+        });
+
+        // Allowing file type
+        const allowedExtensions = /(\.pdf|\.doc|\.docx)$/i;
+    
+        if(!allowedExtensions.exec(this.uploadInput.value)){
+            this.uploadInput.classList.add("is-invalid");
+            this.uploadInput.classList.remove("is-valid");
+            result = false;
+        } else{
+            this.uploadInput.classList.add("is-valid");
+            this.uploadInput.classList.remove("is-invalid");
+        }
+
+        return result;
+    }
     /*
      *  TODO: New method for creating the validation
      */
